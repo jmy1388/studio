5d317fcba548ee2d25a8199fd57426de9d5d526a
myung gon jung <jyworks2030@gmail.com>
2025-11-15 11:43:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6149159118-eb1fb",
+  "appId": "1:54982167170:web:286147830287cca029029b",
+  "apiKey": "AIzaSyButJ1lIHZNN9EcBXNjOu4kT_vNaBPVYWI",
+  "authDomain": "studio-6149159118-eb1fb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "54982167170"
+};
