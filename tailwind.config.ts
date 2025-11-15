9b8e44a4b6ec9022139898f76392782ac53ad85c
myung gon jung <jyworks2030@gmail.com>
2025-11-15 11:40:09 +0000

    Noto Sans KR로 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Noto Sans KR', 'sans-serif'],
+        headline: ['Noto Sans KR', 'serif'],
         code: ['monospace'],
       },
       colors: {
