563647e4a51013600612c3b24b606e9dbbae2752
myung gon jung <jyworks2030@gmail.com>
2025-11-18 11:51:47 +0000

    폰트 일관성 유지

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
+        body: ['Noto Sans KR', 'sans-serif'],
         headline: ['Playfair Display', 'serif'],
         code: ['monospace'],
       },
